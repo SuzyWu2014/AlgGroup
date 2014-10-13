478a73ad3086710d3ba8f0d9413a777b31e8fdb4
Nick Voigt <starboardtitan@gmail.com>
2014-10-13 15:22:54 -0700

    Makefile creation and alg1.c created

---
+++ proj1/alg1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <utime.h>
+#include <string.h>
+#include <dirent.h>
+#include <math.h>
+#include <sys/un.h>
+
+
+int main(){
+ int i, j, k;
+
+ 
+
+return(0);
+}
