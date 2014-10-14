8da523f5bc6d78f295c920fc268b7629656b8e41
Nick Voigt <starboardtitan@gmail.com>
2014-10-13 23:50:34 -0700

    Working Algorithm 1
    Created a working algorithm 1 implementation. Currently has hard coded
    values. TODO change to non-hard coded values.

--- proj1/alg1.c
+++ proj1/alg1.c
@@ -11,8 +11,25 @@
 
 int main(){
  int i, j, k;
+ int m[5] = {-2, -1, 0, 1, 2};
+ int b[5] = {9, 27, 54, 95, 96};
+ int v[5] = {1, 1, 1, 1, 1};
 
- 
+ for(i=1; i<5; i++){
+     for(j=0; j<i; j++){
+         for(k=i+1; k<6; k++){
+             if(m[j]*(b[j]-b[k])+b[j]*(m[k]-m[j])>m[i]*(b[j]-b[k])+b[i]*(m[k]-m[j])){
+                 //line is not visible
+                 v[i] = 0;
+             }
+         }
+     }
+ }
 
+ printf("Visible Lines:");
+ for(i=0;i<5;i++){
+     printf(" %d,", v[i]);
+ }
+printf("\n");
 return(0);
 }
